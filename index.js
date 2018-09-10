c1525f06599237711f68917cd81a5eca1b0d2d76
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 14:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ let neighborhoodId = 0
 let customerId = 0
 class Neighborhood {
   constructor(name){
-    this.name = name 
+    this.name = name
     this.id = neighborhoodId++
+    store.neighborhoods.push(this)
   }
 }
 
 class Customer {
   constructor(name, neighborhoodId) {
-    this.name = name 
+    this.name = name
     this.neighborhoodId = neighborhoodId
     this.id = customerId++
+    store.customers.push(this)
   }
 }
 
@@ -25,15 +27,17 @@ class Meal {
     this.title = title
     this.price = price
     this.id = mealId++
+    store.meals.push(this)
   }
 }
 
 
 class Delivery {
   constructor(mealId,neighborhoodId,customerId){
-    this.mealId = mealId 
+    this.mealId = mealId
     this.neighborhoodId = neighborhoodId
     this.customerId = customerId
     this.id = deliveryId++
+    store.deliveries.push(this)
   }
 }
