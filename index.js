20104043a65db14fc617ddf17aa066caa60ecb11
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 14:02:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let mealId = 0
+
+class Meal {
+  constructor(title,price) {
+    this.title = title 
+    this.price = price 
+    this.id = mealId++
+  }
+}
