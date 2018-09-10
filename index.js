e6bb7d55cfea618f3ba6dbeed324cf23f3f1caae
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 14:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let mealId = 0
+let deliveryId = 0
+let neighborhoodId = 0
+let customerId = 0
+class Neighborhood {
+  constructor(name){
+    this.name = name 
+    this.id = neighborhoodId++
+  }
+}
+
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.name = name 
+    this.neighborhoodId = neighborhoodId
+    this.id = customerId++
+  }
+}
+
 
 class Meal {
   constructor(title,price) {
-    this.title = title 
-    this.price = price 
+    this.title = title
+    this.price = price
     this.id = mealId++
+  }
+}
+
+
+class Delivery {
+  constructor(mealId,neighborhoodId,customerId){
+    this.mealId = mealId 
+    this.neighborhoodId = neighborhoodId
+    this.customerId = customerId
+    this.id = deliveryId++
   }
 }
